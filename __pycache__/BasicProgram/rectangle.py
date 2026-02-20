67a5c840eb59d1a4e8b0c9ab5bdec0601376eca1
SandipakumarBiswal038-debug <sandipkumarbiswal038@gmail.com>
2026-02-20 12:25:38 +0530

    test

---
+++ __pycache__/BasicProgram/rectangle.py
@@ -0,0 +1,14 @@
+"""
+Wap take rectangle length and breadth from keyboard
+display lenghth area and perimeter
+"""
+print("enter rectangle lenghth")
+L=int(input())
+print("enter rectangle breadth")
+B=int(input())
+ar=L*B
+pr=2*(L+B)
+print("lenghth=",L)
+print("breadth=",B)
+print("area=",ar)
+print("perimeter=",pr)
