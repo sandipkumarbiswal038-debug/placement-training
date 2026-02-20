e1561d5c2fad65ee0f6d36d9ba0d16b608b52d12
SandipakumarBiswal038-debug <sandipkumarbiswal038@gmail.com>
2026-02-20 12:29:03 +0530

    test

--- __pycache__/BasicProgram/rectangle.py
+++ __pycache__/BasicProgram/rectangle.py
@@ -12,3 +12,16 @@ print("lenghth=",L)
 print("breadth=",B)
 print("area=",ar)
 print("perimeter=",pr)
+
+"""
+o/p:
+enter rectangle lenghth
+3
+enter rectangle breadth
+4
+lenghth= 3
+breadth= 4
+area= 12
+perimeter= 14
+
+"""
